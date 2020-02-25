cd47304a7ad2f401a7220ffe35f6c59636e8fe3f
Gibran Estrada <gibran9@gmail.com>
2020-02-25 12:15:45 -0500

    Updates

---
+++ precache-manifest.9fd89a8a2881c2af77c6263d16eb55dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5401a11388687a89b022adf4ed6806b5",
+    "url": "/doc-editor/index.html"
+  },
+  {
+    "revision": "92b1a04146cee618d282",
+    "url": "/doc-editor/static/css/2.5cf75900.chunk.css"
+  },
+  {
+    "revision": "87a5f50fc3cba40a859f",
+    "url": "/doc-editor/static/css/main.2396f65f.chunk.css"
+  },
+  {
+    "revision": "92b1a04146cee618d282",
+    "url": "/doc-editor/static/js/2.2d2a156d.chunk.js"
+  },
+  {
+    "revision": "87a5f50fc3cba40a859f",
+    "url": "/doc-editor/static/js/main.2bd6b881.chunk.js"
+  },
+  {
+    "revision": "bb96dd6dc3eadf54444f",
+    "url": "/doc-editor/static/js/runtime-main.d2b38624.js"
+  }
+]);
